c7d69427a694511f56094861c29a6a37e5163542
Vais008 <vaishnavivg008@gmail.com>
2025-04-08 21:55:52 +0530

    features section added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://5e8aca8b463e210a9d231168adbe4f01@o4509118316085248.ingest.us.sentry.io/4509118320345088",
+  integrations:[
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    })
+  ]
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
